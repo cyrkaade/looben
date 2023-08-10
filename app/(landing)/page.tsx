171240f9b9039892e51bf04b6b9b2bb1a13a8cd9
Arslan Akishev <jummdust@gmail.com>
2023-08-10 16:10:19 +0600

    added login/registration

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,23 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const LandingPage = () => {
   return (
-    <div>Landing Page (unprotected)</div>
+    <div>Landing Page (unprotected)
+        <div>
+            <Link href="/sign-in">
+            <Button>
+                Login
+            </Button>
+            </Link>
+            <Link href="/sign-up">
+            <Button>
+                Register
+            </Button>
+            </Link>
+        </div>
+    </div>
   )
 }
 
