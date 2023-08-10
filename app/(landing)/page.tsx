7274e1148892331ede0acf7c1b68b056892e71c0
Arslan Akishev <jummdust@gmail.com>
2023-08-10 15:15:46 +0600

    folder structure

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+const LandingPage = () => {
+  return (
+    <div>Landing Page (unprotected)</div>
+  )
+}
+
+export default LandingPage
